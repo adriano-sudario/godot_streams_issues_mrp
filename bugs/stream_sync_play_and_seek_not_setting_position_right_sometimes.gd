52ac1893e42a6e3b0d384665984e51d2e00f7914
Adriano Sudário <sudario.adriano@gmail.com>
2024-12-11 16:56:53 -0300

    added seek on reproduction

--- bugs/stream_sync_play_and_seek_not_setting_position_right_sometimes.gd
+++ bugs/stream_sync_play_and_seek_not_setting_position_right_sometimes.gd
@@ -6,7 +6,10 @@ extends Control
 func _process(_delta):
 	if Input.is_action_just_pressed("space"):
 		if audio_player.playing:
-			audio_player.stop()
+			if Input.is_key_pressed(KEY_SHIFT):
+				audio_player.seek(41)
+			else:
+				audio_player.stop()
 		else:
 			if Input.is_key_pressed(KEY_SHIFT):
 				audio_player.play(41)
@@ -15,5 +18,8 @@ func _process(_delta):
 	
 	if Input.is_action_just_pressed("1"):
 		audio_player.play(audio_player.get_playback_position())
+	
+	if Input.is_action_just_pressed("2"):
+		audio_player.seek(audio_player.get_playback_position())
 	
 	playback_position_label.text = "playback position: %0.2f" % audio_player.get_playback_position()
