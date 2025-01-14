d6dfe562f27e00b5b87021dada4b63ee46b7a6a5
Adriano Sudário <sudario.adriano@gmail.com>
2025-01-13 21:42:29 -0300

    adjustments to add more case scenarios

--- main.gd
+++ main.gd
@@ -5,10 +5,10 @@ func _on_issue_one_button_pressed():
 	get_tree().change_scene_to_file("res://bugs/interactive_stream_get_playback_position_returns_always_zero.tscn")
 
 func _on_issue_two_button_pressed():
-	get_tree().change_scene_to_file("res://bugs/stream_sync_play_not_setting_position_right_sometimes.tscn")
+	get_tree().change_scene_to_file("res://bugs/stream_sync_play_and_seek_not_setting_position_right_sometimes.tscn")
 
 func _on_proposal_one_button_pressed():
 	get_tree().change_scene_to_file("res://unexpected_behaviors/stream_sync_not_adding_through_code.tscn")
-	
+ 
 func _on_proposal_two_button_pressed():
 	get_tree().change_scene_to_file("res://unexpected_behaviors/stream_sync_stops_when_set.tscn")
--- unexpected_behaviors/stream_sync_stops_when_set.gd
+++ unexpected_behaviors/stream_sync_stops_when_set.gd
@@ -3,6 +3,7 @@ extends Control
 var is_guitarrinha1 := true
 var guitarrinha1 := load("res://sounds/guitarrinha1.mp3")
 var guitarrinha2 := load("res://sounds/guitarrinha2.mp3")
+var is_null := false
 
 @onready var audio_player = $AudioPlayer
 @onready var playback_position_label = $PlaybackPositionLabel
@@ -17,7 +18,20 @@ func _process(_delta):
 	if Input.is_action_just_pressed("1"):
 		toggle_guitarrinha()
 	
+	if Input.is_action_just_pressed("2"):
+		toggle_null()
+	
 	playback_position_label.text = "playback position: %0.2f" % audio_player.get_playback_position()
+
+func toggle_null():
+	is_null = not is_null
+	
+	if is_null:
+		audio_player.stream.set_sync_stream(3, null)
+	elif is_guitarrinha1:
+		audio_player.stream.set_sync_stream(3, guitarrinha1)
+	else:
+		audio_player.stream.set_sync_stream(3, guitarrinha2)
 
 func toggle_guitarrinha():
 	if is_guitarrinha1:
